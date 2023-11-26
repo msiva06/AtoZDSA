0c2329cc54ddad922a29ffb7d43e2c3b79df7d48
msiva06 <m.sivasubramanian06@gmail.com>
2023-11-25 20:11:56 -0500

    Longest common substring added

---
+++ DP/Problem_27.java
@@ -0,0 +1,21 @@
+// Longest common substring
+public class Problem_27 {
+    public static int lcs(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n+1][m+1];
+        int ans = Integer.MIN_VALUE;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = Math.max(dp[i][j],ans);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
