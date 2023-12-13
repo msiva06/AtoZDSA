e7db2a3364600e548bf3127657338e82d8c31dcc
msiva06 <m.sivasubramanian06@gmail.com>
2023-12-12 20:22:47 -0500

    Trie Delete operation added

---
+++ Trie/Problem_03.java
@@ -0,0 +1,45 @@
+package Trie;
+
+// Trie Delete operation
+public class Problem_03 {
+    public static TrieNode deleteWord(TrieNode root, String word) {
+        TrieNode node = root;
+        return deleteWordHelper(node,word,0);
+    }
+
+    public static TrieNode deleteWordHelper(TrieNode root,String word, int depth){
+        TrieNode node = root;
+        if(node == null)
+            return null;
+        if(depth == word.length()){
+             if(node.isEnd){
+                node.isEnd = false;
+            }
+            if(isEmpty(node))
+            {
+                node = null;
+            }
+           
+            return node;
+        }
+        char c = word.charAt(depth);
+        int idx = c - 'a';
+        node.children[idx] = deleteWordHelper(node.children[idx], word, depth+1);
+        if(isEmpty(node) && node.isEnd == false)
+            node = null;
+        return node;
+
+        
+        
+        
+    }
+
+    public static boolean isEmpty(TrieNode root){
+        TrieNode node = root;
+        for(int i = 0; i < 26; i++){
+            if(node.children[i] != null)
+                return false;
+        }
+        return true;
+    }
+}
