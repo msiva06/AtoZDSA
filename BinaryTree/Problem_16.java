ffe1c7dd0edf75083ae2ca25812d1272e0efcdcc
msiva06 <m.sivasubramanian06@gmail.com>
2023-12-29 16:02:24 -0500

    Is height balanced binary tree added

---
+++ BinaryTree/Problem_16.java
@@ -0,0 +1,25 @@
+package BinaryTree;
+
+// Is height balanced binary tree
+public class Problem_16 {
+    
+    public static boolean isBalancedBT(TreeNode<Integer> root) {
+        if(checkBalanced(root) == -1)
+            return false;
+        return true;
+    }
+
+    public static int checkBalanced(TreeNode<Integer> root){
+        if(root == null)
+            return 0;
+        int lt = checkBalanced(root.left);
+        if(lt == -1)
+            return -1;
+        int rt = checkBalanced(root.right);
+        if(rt == -1)
+            return -1;
+        if(Math.abs(lt-rt) > 1)
+            return -1;
+        return 1 + Math.max(lt,rt);
+    }
+}
