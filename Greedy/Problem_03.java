e322be7811f74f247c59617c9a4f101c54781ed7
msiva06 <m.sivasubramanian06@gmail.com>
2024-01-17 11:27:51 -0500

    Min number of platforms required for a railway

---
+++ Greedy/Problem_03.java
@@ -0,0 +1,32 @@
+package Greedy;
+
+import java.util.Arrays;
+
+// Min number of platforms required for a railway
+public class Problem_03 {
+    public static int calculateMinPatforms(int arr[], int dep[], int n) {
+        Arrays.sort(arr); 
+        Arrays.sort(dep); 
+  
+        int plat_needed = 1, result = 1; 
+        int i = 1, j = 0; 
+  
+        while (i < n && j < n) { 
+        
+            if (arr[i] <= dep[j]) { 
+                plat_needed++; 
+                i++; 
+            } 
+  
+            else if (arr[i] > dep[j]) { 
+                plat_needed--; 
+                j++; 
+            } 
+  
+            if (plat_needed > result) 
+                result = plat_needed; 
+        } 
+  
+        return result; 
+    }
+}
